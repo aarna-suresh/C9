e19d0809f6625847cad4093a02604f61e22fc050
aarna-suresh <94156873+aarna-suresh@users.noreply.github.com>
2021-12-20 17:45:13 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var box;
+
+
+function setup() {
+  createCanvas(400,400);
+
+box = createSprite(200,200,20,20);
+box.shapeColor = "pink";
+}
+
+function draw() 
+{
+  background(30);
+
+drawSprites();
+}
+
+
+
+
